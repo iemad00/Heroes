330797619094003a339366b40c76fd5010251612
Emad Albalawi <91331130+iemad00@users.noreply.github.com>
2024-05-23 23:19:47 +0300

    Power Budge instead of list

--- src/app/components/heroes/heroes-list/heroes-list.component.ts
+++ src/app/components/heroes/heroes-list/heroes-list.component.ts
@@ -10,10 +10,29 @@ export class HeroesListComponent {
 
   sortBy: 'name'|'power' = 'name';
 
-  heroes = [{
+  heroes = [
+    {
     name: "Emad",
-    powers: ['Angular', 'Nodejs'],
+    powers: ['Angular', 'Nodejs', 'Python'],
     rate: 3.5
-  }];
+    },
+    {
+      name: "Super Man",
+      powers: ['Fly', 'Power', 'Nodejs', 'React', 'Strong'],
+      rate: 5
+    },
 
+    {
+      name: "Ahmen Man",
+      powers: ['Fly', 'Power'],
+      rate: 5
+    }
+  ];
+
+
+  get filteredHeroes() {
+    return this.heroes.sort((a, b) => {
+      return this.sortBy === 'name' ? a.name.localeCompare(b.name) : b.powers.length - a.powers.length;
+    });
+  }
 }
