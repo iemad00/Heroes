2518e1fde7c612c18584faea8730bfd61c449601
Emad Albalawi <91331130+iemad00@users.noreply.github.com>
2024-05-23 21:22:17 +0300

    Fix login page style

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -8,10 +8,15 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 
 export class LoginComponent {
+  loading: boolean = false;
 
   signInForm = new FormGroup({
     email: new FormControl('', Validators.email),
     password: new FormControl('', Validators.required),
   });
 
+
+  signIn(){
+
+  }
 }
