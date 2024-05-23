c94f850e4b423491e9ad537d0aa9bf5745a315cc
Emad Albalawi <91331130+iemad00@users.noreply.github.com>
2024-05-23 18:51:48 +0300

    Auth Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisrerComponent } from './components/auth/regisrer/regisrer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisrerComponent
   ],
   imports: [
     BrowserModule,
