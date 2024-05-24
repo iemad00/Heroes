fbaa9dda1d703cf6082c64e920ee79741953af03
Emad Albalawi <91331130+iemad00@users.noreply.github.com>
2024-05-24 20:17:08 +0300

    change menu items

--- src/app/components/common/navbar/navbar.component.ts
+++ src/app/components/common/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -6,15 +6,23 @@ import { AuthService } from 'src/app/services/auth.service';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   public isCollapsed = true;
 
   menuItems: any[] = [
     { path: 'heroes', title: 'Hero List'},
-    { path: 'my-profile', title: 'My Profile'},
   ];
 
-  constructor(private authService: AuthService){}
+  constructor(public authService: AuthService){}
+
+  ngOnInit(): void {
+    if(this.authService.isAuthenticated())
+      this.menuItems.push({ path: 'my-profile', title: 'My Profile'})
+    else{
+      this.menuItems.push({ path: 'login', title: 'Login'})
+      this.menuItems.push({ path: 'register', title: 'Create a new Account'})
+    }
+  }
 
 
   logout(){
