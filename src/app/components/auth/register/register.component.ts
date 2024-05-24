c76b803be55f8a9f08faa03fabca88c69c04ba68
Emad Albalawi <91331130+iemad00@users.noreply.github.com>
2024-05-24 08:05:36 +0300

    remove unused code

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormArray, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,53 +7,5 @@ import { FormGroup, FormControl, Validators, FormArray, AbstractControl } from '
 })
 export class RegisterComponent{
 
-  loading: boolean = false;
-
-  signUpForm: FormGroup = new FormGroup({
-    heroName: new FormControl('', [Validators.minLength(4), Validators.required]),
-    age: new FormControl('', [Validators.required]),
-    gender: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.email, Validators.required]),
-    password: new FormControl('', [Validators.pattern(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[.@$!%*#?&^_-]).{8,}/)]),
-    confirmPassword: new FormControl('', Validators.required),
-    powers: new FormArray([
-      this.createPower()
-    ]),
-  });;
-
-
-  isValid(formControl: AbstractControl){
-    return !(!formControl.valid && formControl.touched);
-  }
-
-  get powers(): FormArray {
-    return this.signUpForm.get('powers') as FormArray;
-  }
-
-  isPasswordMatch() {
-    return !(this.signUpForm.controls['confirmPassword'].touched && this.signUpForm.value.confirmPassword !== this.signUpForm.value.password);
-  }
-
-  createPower(): FormGroup {
-    return new FormGroup({
-      power: new FormControl('', Validators.required)
-    });
-  }
-
-  addPower(): void {
-    this.powers.push(this.createPower());
-  }
-
-  removePower(index: number): void {
-    this.powers.removeAt(index);
-  }
-
-  isFormValid(){
-    return this.signUpForm.valid && (this.signUpForm.value.password == this.signUpForm.value.confirmPassword)
-  }
-
-  signUp(){
-
-  }
 
 }
