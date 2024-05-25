5afc444749dbfedb21709da67e5e1287b6dff1f0
Emad Albalawi <91331130+iemad00@users.noreply.github.com>
2024-05-25 06:02:56 +0300

    Register Component test

--- src/app/components/auth/register/register.component.spec.ts
+++ src/app/components/auth/register/register.component.spec.ts
@@ -1,15 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
 import { RegisterComponent } from './register.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [RegisterComponent],
+      imports: [RouterTestingModule],
+      schemas: [NO_ERRORS_SCHEMA] // Ignore unrecognized elements
+    }).compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [RegisterComponent]
-    });
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +24,20 @@ describe('RegisterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain a link to the login page', () => {
+    const loginLink = fixture.debugElement.query(By.css('a[routerLink="/login"]')).nativeElement;
+    expect(loginLink.textContent).toContain('Login in');
+  });
+
+  it('should contain a link to the heroes list page', () => {
+    const heroesLink = fixture.debugElement.query(By.css('a[routerLink="/heroes"]')).nativeElement;
+    expect(heroesLink.textContent).toContain('Go to heroes list');
+  });
+
+  it('should contain the app-create-hero-form component', () => {
+    const formComponent = fixture.debugElement.query(By.css('app-create-hero-form'));
+    expect(formComponent).toBeTruthy();
   });
 });
